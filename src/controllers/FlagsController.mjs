d9d61f8d7079c303abbfb06e995c9b6f937f0cff
Emmanuel Fregoso <luis.fregoso4195@alumnos.udg.mx>
2025-03-25 11:29:09 -0600

    clase 25/03/2025

--- src/controllers/FlagsController.mjs
+++ src/controllers/FlagsController.mjs
@@ -1,40 +1,65 @@
 import Joi from 'joi';
+import { StoreFlag } from '../actions/StoreFlag.mjs';
+import { Flag } from '../models/Flag.mjs';
 
-export function index(req, res) {
-    const allFlags = [
-        { id: 1, name: 'flag1' },
-        { id: 2, name: 'flag2' },
-        { id: 3, name: 'flag3' },
-    ];
-    res.json({ message: 'Flags index', data: allFlags });
+export async function index(req, res) {
+    const query = req.query;
+    if (query.limit && query.offset) {
+        const limit = parseInt(query.limit);
+        const offset = parseInt(query.offset);
+        const flags = await Flag.findAll({ limit, offset });
+        return res.json({ message: 'Flags index', data: flags });
+    }
+    if(query.status){
+        const flags = await Flag.findAll({ where: { status: query.status } });
+        return res.json({ message: 'Flags index', data: flags });
+    }
+    const flags = await Flag.findAll();
+    res.json({ message: 'Flags index', data: flags, pagination: { total: flags.length } });
 }
 
-export function show(req, res) {
-    const flag = { id: 1, name: 'flag1' };
+export async function show(req, res) {
+    const idParam = req.params.id;
+
+    const flag = await Flag.findByPk(idParam);
     res.json({ message: 'Flags show', data: flag });
 }
 
-export function store(req, res) {
+export async function store(req, res) {
     const schema = Joi.object({
-        name: Joi.string().required(),
-        description: Joi.string().required()
+        name: Joi.number().required(),
+        status: Joi.string().required()
     })
     const { error } = schema.validate(req.body);
     if (error) {
         return res.status(400).json({ message: error.details[0].message });
     }
-    
-    const newFlag = { id: 10, name: req.body.name, description: req.body.description };
+    const newFlag = await StoreFlag(req.body);
     res.status(201).json({ message: 'Flags store', data: newFlag });
+
 }
 
 
-export function update(req, res) {
-    
+export async function update(req, res) {
+    const idParam = req.params.id;
+    const schema = Joi.object({
+        name: Joi.number().required(),
+        status: Joi.string().required()
+    })
+    const { error } = schema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ message: error.details[0].message });
+    }
+    const updatedFlag = await Flag.update(req.body, { where: { id: idParam } });
+
+
     res.json({ message: 'Flags update' });
 }
 
-export function destroy(req, res) {
+export async function destroy(req, res) {
+
+    const idParam = req.params.id;
+    await Flag.destroy({ where: { id: idParam } });
 
     res.json({ message: 'Flags destroy' });
 }
--- src/routes/routes.mjs
+++ src/routes/routes.mjs
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { index, store } from "../controllers/FlagsController.mjs";
+import { destroy, index, show, store, update } from "../controllers/FlagsController.mjs";
 
 
 const router = Router();
 
 router.get("/flags", index);
+router.get("/flags/:id", show);
 router.post("/flags", store);
+router.post("/flags/:id", update);
+router.delete("/flags/:id", destroy);
 
 
 
