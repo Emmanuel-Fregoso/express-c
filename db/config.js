b157b80776b855373b4bc104ed25b61679035470
Emmanuel Fregoso <luis.fregoso4195@alumnos.udg.mx>
2025-05-06 11:54:28 -0600

    Update Sequelize connection string to use environment variable for password

--- db/config.js
+++ db/config.js
@@ -3,7 +3,7 @@ import pg from 'pg';
 
 console.log(process.env.DB_PASSWORD)
 
-export const db = new Sequelize('postgresql://postgres.srksqeedpoyfcoqamket:7ecdz5pjL8KX4YJN@aws-0-us-east-2.pooler.supabase.com:6543/postgres',
+export const db = new Sequelize(`postgresql://postgres.srksqeedpoyfcoqamket:${process.env.DB_PASSWORD}@aws-0-us-east-2.pooler.supabase.com:6543/postgres`,
     {
         dialect: 'postgres',
         dialectModule: pg,
