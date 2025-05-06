02080ba3df372d9e9b5c6b605cc67a48d161cdd8
Emmanuel Fregoso <luis.fregoso4195@alumnos.udg.mx>
2025-05-06 11:23:07 -0600

    Add dialectModule configuration for Sequelize PostgreSQL connection

--- db/config.js
+++ db/config.js
@@ -1,3 +1,8 @@
 import { Sequelize } from 'sequelize';
+import pg from 'pg';
 
-export const db = new Sequelize(`postgresql://postgres:${process.env.DB_PASSWORD}@db.srksqeedpoyfcoqamket.supabase.co:5432/postgres`);
+export const db = new Sequelize(`postgresql://postgres:${process.env.DB_PASSWORD}@db.srksqeedpoyfcoqamket.supabase.co:5432/postgres`,
+    {
+        dialectModule: pg
+    }
+);
