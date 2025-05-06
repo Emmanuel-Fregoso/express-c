b36d23a800492e72c440ebaf8b4ff4426f9ee5de
Emmanuel Fregoso <luis.fregoso4195@alumnos.udg.mx>
2025-05-06 11:29:13 -0600

    Refactor Sequelize connection to use separate parameters for host, port, and dialect

--- db/config.js
+++ db/config.js
@@ -1,8 +1,13 @@
 import { Sequelize } from 'sequelize';
 import pg from 'pg';
 
-export const db = new Sequelize(`postgresql://postgres:${process.env.DB_PASSWORD}@db.srksqeedpoyfcoqamket.supabase.co:5432/postgres`,
+console.log(process.env.DB_PASSWORD)
+
+export const db = new Sequelize('postgres','postgres', process.env.DB_PASSWORD,
     {
+        host: 'db.srksqeedpoyfcoqamket.supabase.co',
+        port: 5432,
+        dialect: 'postgres',
         dialectModule: pg
     }
 );
